2cdd5f62a23d176e51a7e645881544bd2aaf7960
Marek Kacmar <kacmar.marek@gmail.com>
2023-03-07 18:21:50 +0100

    Reverting changes

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  publicPath: '/dp-app/',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        vue$: 'vue/dist/vue.esm-bundler.js'
+      }
+    }
+  },
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableLegacy: false,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true
+    }
+  },
+  devServer: {
+    proxy: 'http://localhost:8081'
+  }
+}
